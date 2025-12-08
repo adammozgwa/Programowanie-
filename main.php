ae2a7a41c96da19bccf1d494bed203f416bd6853
adammozgwa <adimozgwa@gmail.com>
2025-12-08 13:27:54 +0100

    Add files via upload

---
+++ main.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        header{
+            width: 100%;
+            height: 20%;
+            background-color:blue;
+        }
+        aside{
+            float: left;
+            width: 20%;
+            height: 80%;
+            background-color:red;
+            
+        }
+        main{
+            width:80%;
+            height: 60%;
+            background-color:gray;
+        }
+        footer{
+            clear: both;
+        }
+        html, body{
+            height: 100%;
+            width: 100%;
+            
+        }
+        
+    </style>
+</head>
+<body>
+    <header><h1>Poznaj sie</h1></header>
+
+    <aside>
+       <button>1</button><br>
+        <button>2</button><br>
+       <button>3</button><br>
+
+    </aside>
+
+    <main>
+        
+        <footer>
+        <input type="text">
+        <button>Wyślij</button>
+    </footer>
+    </main>
+    
+    
+
+</body>
+</html>
